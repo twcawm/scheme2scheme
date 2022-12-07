60da425cf5fd96eab4a0e28f7f64c0d1788d7c1c
twcawm <twcawm@gmail.com>
2022-12-07 15:28:10 -0500

    test number evaluation

--- eval.scm
+++ eval.scm
@@ -5,3 +5,7 @@
                )
              )
 )
+
+(define sre5 (scheme-report-environment 5)) ; shorthand for test environment
+(eval 4 sre5) ; should evaluate to 4
+(eval "test" sre5) ; should break
