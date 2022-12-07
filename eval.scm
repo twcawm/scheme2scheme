a1f89a3cc879082380c6d5d157483281fdf8d99b
twcawm <twcawm@gmail.com>
2022-12-07 15:52:40 -0500

    implement quote

--- eval.scm
+++ eval.scm
@@ -2,10 +2,12 @@
 (define eval (lambda (exp env)
                (cond
                  ((number? exp) exp)
+                 ((eq? (car exp) 'quote) (car (cdr exp))) ;we're implicitly expecting a pair here; need to improve this
                )
              )
 )
 
 (define sre5 (scheme-report-environment 5)) ; shorthand for test environment
 (eval 4 sre5) ; should evaluate to 4
+(eval '(quote 3) sre5) ; if we just use (quote 3), we're just eval'ing a number.  to eval (quote 3) literally, need '(quote 3)
 (eval "test" sre5) ; should break
