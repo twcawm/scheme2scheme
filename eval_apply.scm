0e9f7b6bc1152dc7143217a1b72072520fe3c855
twcawm <twcawm@gmail.com>
2022-12-08 13:39:40 -0500

    implement frame as a pair of variable and value list

--- eval_apply.scm
+++ eval_apply.scm
@@ -8,7 +8,17 @@
 (define (first-frame env) (car env)) ;environments have a nested structure.  the innermost frame is the car of the environment
 (define (enclosing-environment env) (cdr env)) ;likewise, the enclosing environment is the cdr
 
+(define (make-frame variables values)
+  (cons variables values)) ;frame: a pair of lists.  car is a list of variable names.  cons is a list of their values. frame: ((variables) values)
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
 
-the-empty-environment
-(first-frame the-empty-environment) ;apparently invalid to call frame selectors on empty environment
-(enclosing-environment the-empty-environment) ;apparently invalid to call frame selectors on empty environment
+
+
+(define frame0 (make-frame (list 'x 'y) (list 5 6 ))) (add-binding-to-frame! 'z 7 frame0) (display frame0) (frame-variables frame0) (frame-values frame0) ;illustrates building a frame
+
+;(first-frame the-empty-environment) ;apparently invalid to call frame selectors on empty environment
+;(enclosing-environment the-empty-environment) ;apparently invalid to call frame selectors on empty environment
