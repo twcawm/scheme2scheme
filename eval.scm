9253cad40ef48072cec364c638ffe9a233217e57
twcawm <twcawm@gmail.com>
2022-12-07 20:28:40 -0500

    add string capability

--- eval.scm
+++ eval.scm
@@ -2,12 +2,15 @@
 (define eval (lambda (exp env)
                (cond
                  ((number? exp) exp)
+                 ((string? exp) exp) ; for a number or string, evaluates to itself
                  ((eq? (car exp) 'quote) (car (cdr exp))) ;we're implicitly expecting a pair here; need to improve this
+                 ;btw, for quote, it's cadr instead of cdr because (cdr '(quote abc)) evaluates to (abc) (list of one element) when we really just want the first element of that list.
                )
              )
 )
 
 (define sre5 (scheme-report-environment 5)) ; shorthand for test environment
 (eval 4 sre5) ; should evaluate to 4
-(eval '(quote 3) sre5) ; if we just use (quote 3), we're just eval'ing a number.  to eval (quote 3) literally, need '(quote 3)
-(eval "test" sre5) ; should break
+(eval '(quote abc) sre5) ; if we just use (quote 3), we're just eval'ing a number.  to eval (quote 3) literally, need '(quote 3)
+(eval '(quote (quote (quote abc) ) ) sre5) ; not sure
+(eval "test" sre5) ; string works now
