af6f729b127131bfbfa6705da6bcf8a74c3ec9d5
twcawm <twcawm@gmail.com>
2022-12-07 23:57:06 -0500

    start implementing combinations - I suspect we still need a way to lookup primitives

--- eval.scm
+++ eval.scm
@@ -1,11 +1,36 @@
 #lang sicp
+
+(define evlist 
+  (lambda (l env)
+    (display "in evlist")
+    (cond ((eq? l '() ) '() )
+          (else
+           (cons
+            (eval (car l) env)
+            (evlist (cdr l) env)
+           )
+          )
+     )
+   )
+)
+;next, write evcond
+;and probably need the primitive apply...?
 (define eval (lambda (exp env)
                (cond
                  ((number? exp) exp)
                  ((string? exp) exp) ; for a number or string, evaluates to itself
-                 ((eq? (car exp) 'quote) (car (cdr exp))) ;we're implicitly expecting a pair here; need to improve this
+                 ((and (pair? exp) (eq? (car exp) 'quote)) (car (cdr exp))) ;we're implicitly expecting a pair here; need to improve this
                  ;btw, for quote, it's cadr instead of cdr because (cdr '(quote abc)) evaluates to (abc) (list of one element) when we really just want the first element of that list.
-                 ((eq? (car exp) 'lambda) (display "in lambda def") (list 'closure (cdr exp) env)) ; start lambda definition
+                 ((and (pair? exp) (eq? (car exp) 'lambda)) (display "in lambda def") (list 'closure (cdr exp) env)) ; start lambda definition
+                 ((and (pair? exp) (eq? (car exp) 'cond)) (display "in cond")
+                  (eval (cdr exp) env)) ;we do not have evcond yet - this is incorrect for now
+                 (else ;default: a combination
+                  (display "in combination") (newline)
+                  (apply
+                   (eval (car exp) env)
+                   (evlist (cdr exp) env)
+                  )
+                 )
                )
              )
 )
@@ -16,3 +41,7 @@
 (eval '(quote (quote (quote abc) ) ) sre5) ; not sure
 (eval "test" sre5) ; string works now
 (eval '(lambda (x) (+ x 5)) sre5) ; doesn't really do anything yet, but saves lambda body and env in a "closure"-tagged list , at least
+
+(newline)
+;(eval '(cond ((> 5 4) 6 ) )) ;broken for now
+(eval '(+ 5 6 ) sre5) ;this currently breaks - I think it is because we do not lookup '+ ! so we need to implement lookup
